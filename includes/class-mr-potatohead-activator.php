56e550504b81263e97d2e496f081538ee8277556
Habby Whyte <bahybaj@hotmail.com>
2020-02-05 14:15:00 -0500

    initial commit

---
+++ includes/class-mr-potatohead-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       webdesignbyhabby.com
+ * @since      1.0.0
+ *
+ * @package    Mr_Potatohead
+ * @subpackage Mr_Potatohead/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mr_Potatohead
+ * @subpackage Mr_Potatohead/includes
+ * @author     Habby Whyte <bahybaj@hotmail.com>
+ */
+class Mr_Potatohead_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-mr-potatohead-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       webdesignbyhabby.com
+ * @since      1.0.0
+ *
+ * @package    Mr_Potatohead
+ * @subpackage Mr_Potatohead/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Mr_Potatohead
+ * @subpackage Mr_Potatohead/includes
+ * @author     Habby Whyte <bahybaj@hotmail.com>
+ */
+class Mr_Potatohead_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
